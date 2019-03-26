fdb549aa61e34776aac2d63118bd48e9a2d28b5e
hus <1660846028@qq.com>
2019-03-26 11:55:48 +0800

    现实了model

--- src/main/java/com/lcvc/ebuy_hcs/dao/AdminDao.java
+++ src/main/java/com/lcvc/ebuy_hcs/dao/AdminDao.java
@@ -1,5 +1,7 @@
 package com.lcvc.ebuy_hcs.dao;
 
+import com.lcvc.ebuy_hcs.model.Admin;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,10 +11,11 @@ public class AdminDao {
 	 * 根据账户名和密码去数据库查询，进行登录判断
 	 * @param username 账户名
 	 * @param password 密码
-	 * @return true表示登录成功,false表示登录失败
+	 * @return true表示登录成功,null表示登录失败
 	 */
-	public boolean login(String username,String password){
-		boolean judge=false;//表示登录状态，默认失败
+	public Admin login(String username,String password){
+		/*boolean judge=false;//表示登录状态，默认失败*/
+        Admin admin=null;//null表示登陆失败
 		Connection conn=DBHelper.getConnection();
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
@@ -23,17 +26,65 @@ public class AdminDao {
 			pstmt.setString(2, password);
 			rs=pstmt.executeQuery();
 			if(rs.next()){//如果能够从数据库找到记录
-				judge=true;
+                admin=new Admin();
+                admin.setId(rs.getInt("id"));
+                admin.setName(rs.getString("name"));
+                admin.setPassword(rs.getString("password"));
+                admin.setUsername(rs.getString("username"));
 			}			
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
 			DBHelper.closeConn(conn,pstmt,rs);
 		}
-		return judge;
+		return admin;
 	}
-	
-	
+/*	public String name(String username){
+		*//*boolean judge=false;//表示登录状态，默认失败*//*
+		String name=null;
+		Connection conn=DBHelper.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			String sql="select * from admin where username=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			rs=pstmt.executeQuery();
+			if(rs.next()){//如果能够从数据库找到记录
+				name=rs.getString("name");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			DBHelper.closeConn(conn,pstmt,rs);
+		}
+		return name;
+	}*/
+	/*public Admin getAdmin(String username){
+		*//*boolean judge=false;//表示登录状态，默认失败*//*
+		Admin admin=null;
+		Connection conn=DBHelper.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			String sql="select * from admin where username=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			rs=pstmt.executeQuery();
+			if(rs.next()){//如果能够从数据库找到记录
+				admin=new Admin();
+				admin.setId(rs.getInt("id"));
+				admin.setName(rs.getString("name"));
+				admin.setPassword(rs.getString("password"));
+				admin.setUsername(rs.getString("username"));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			DBHelper.closeConn(conn,pstmt,rs);
+		}
+		return admin;
+	}*/
 	public static void main(String[] args) {
 		AdminDao adminDao=new AdminDao();
 		System.out.println(adminDao.login("admin", "123"));
